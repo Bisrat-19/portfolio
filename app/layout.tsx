a95f66b5db07b063a7f9cb15773c41ae2cabc362
Bisrat Dereje <bdereje996@gmail.com>
2025-08-02 13:30:53 +0300

    icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,24 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Portfolio',
+  description: 'Personal portfolio showcasing my projects and skills',
+  icons: {
+    icon: [
+      {
+        url: '/favicon.svg',
+        type: 'image/svg+xml',
+      },
+      {
+        url: '/favicon.ico',
+        type: 'image/x-icon',
+      },
+      {
+        url: '/images/profile.jpg',
+        type: 'image/jpeg',
+      }
+    ],
+    apple: '/favicon.svg',
+  },
 }
 
 export default function RootLayout({
